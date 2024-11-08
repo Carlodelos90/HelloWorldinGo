c73e79a309b959b7de4c8dadec6f8dcfd9f9020f
Carlo <carlodelos90@gmail.com>
2024-11-08 11:20:16 +0100

    HelloWorld in GO

---
+++ Helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+
+/*
+    Explanation: Go programs start with a package declaration. The main package is the entry point. fmt is a package that provides formatted I/O, and Println prints with a newline.
+*/
